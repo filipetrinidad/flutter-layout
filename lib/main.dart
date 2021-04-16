f0e5370b887877f89f091b629166de87835a470d
filipetrinidad <fipe5766@gmail.com>
2021-04-16 17:11:41 -0300

    code optimization

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Lista(),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.green,
-        ),
       ),
-    ));
+    );
+  }
+}
+
+class Formulario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crir uma transferência'),
+      ),
+    );
+  }
+}
+
 
 class Lista extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(),
-        ItemTransferencia(),
-        ItemTransferencia(),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferecnia(100.0, 200)),
+          ItemTransferencia(Transferecnia(300.0, 400)),
+          ItemTransferencia(Transferecnia(300.0, 1000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blueGrey,
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
+  final Transferecnia _transferecnia;
 
-  final String valor;
-  final String conta;
+  ItemTransferencia(this._transferecnia);
 
-  ItemTransferencia(this.valor, this.conta);
-  
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(conta),
+        title: Text(_transferecnia.valor.toString()),
+        subtitle: Text(_transferecnia.conta.toString()),
       ),
     );
   }
 }
 
+class Transferecnia {
+  final double valor;
+  final int conta;
 
+  Transferecnia(this.valor, this.conta);
+}
